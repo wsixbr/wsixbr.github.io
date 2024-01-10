82282aabc11058aaed4d99e843e4e2a87314de8d
bwsimiyu <biwsimiyu@gmail.com>
2024-01-10 10:36:05 +0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MaterialImports } from './material.import';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ...MaterialImports],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
